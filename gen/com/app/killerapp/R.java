6231e492cd790c42a4a32f12c8d7de7125228001
Vincent Karsten <vincent.karsten@hva.nl>
2013-03-14 17:04:38 +0100

    Added string

--- gen/com/app/killerapp/R.java
+++ gen/com/app/killerapp/R.java
@@ -35,10 +35,12 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_settings=0x7f030001;
+        public static final int activity_map=0x7f030001;
+        public static final int activity_settings=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f090000;
+        public static final int map=0x7f090001;
     }
     public static final class string {
         public static final int action_exit=0x7f060002;
@@ -47,32 +49,36 @@ public final class R {
          */
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060003;
-        public static final int pref_default_display_name=0x7f06000a;
-        public static final int pref_description_social_recommendations=0x7f060008;
+        public static final int pref_default_display_name=0x7f06000c;
+        public static final int pref_description_social_recommendations=0x7f06000a;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f06000c;
+        public static final int pref_header_data_sync=0x7f06000e;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f060006;
+        public static final int pref_header_general=0x7f060008;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f06000f;
-        public static final int pref_ringtone_silent=0x7f060012;
-        public static final int pref_title_add_friends_to_messages=0x7f06000b;
-        public static final int pref_title_display_name=0x7f060009;
-        public static final int pref_title_new_message_notifications=0x7f060010;
-        public static final int pref_title_ringtone=0x7f060011;
-        public static final int pref_title_social_recommendations=0x7f060007;
-        public static final int pref_title_sync_frequency=0x7f06000d;
-        public static final int pref_title_system_sync_settings=0x7f06000e;
-        public static final int pref_title_vibrate=0x7f060013;
+        public static final int pref_header_notifications=0x7f060011;
+        public static final int pref_ringtone_silent=0x7f060014;
+        public static final int pref_title_add_friends_to_messages=0x7f06000d;
+        public static final int pref_title_display_name=0x7f06000b;
+        public static final int pref_title_new_message_notifications=0x7f060012;
+        public static final int pref_title_ringtone=0x7f060013;
+        public static final int pref_title_social_recommendations=0x7f060009;
+        public static final int pref_title_sync_frequency=0x7f06000f;
+        public static final int pref_title_system_sync_settings=0x7f060010;
+        public static final int pref_title_vibrate=0x7f060015;
         /**  End Main Activity 
  Start settings Activity 
          */
-        public static final int settings=0x7f060004;
-        public static final int title_activity_settings=0x7f060005;
+        public static final int settings=0x7f060005;
+        /**  End Settings Activity 
+         */
+        public static final int title_activity_map=0x7f060006;
+        public static final int title_activity_menu=0x7f060004;
+        public static final int title_activity_settings=0x7f060007;
     }
     public static final class style {
         /** 
