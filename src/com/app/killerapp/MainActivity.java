f5a12442f9ecdd3e370e670ad4b0c4fd1b6e1ec9
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-03-14 13:30:47 +0100

    added settings activity

--- src/com/app/killerapp/MainActivity.java
+++ src/com/app/killerapp/MainActivity.java
@@ -18,7 +18,9 @@ package com.app.killerapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -58,12 +60,26 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+		case R.id.action_exit:
+			
+			return true;
+		case R.id.action_settings:
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
