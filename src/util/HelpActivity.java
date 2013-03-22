3ba72422e4f9774034790573a080372837c1194c
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-03-22 18:30:43 +0100

    Modifed help activity text

---
+++ src/util/HelpActivity.java
@@ -0,0 +1,26 @@
+package util;
+
+import com.app.killerapp.R;
+import com.app.killerapp.R.layout;
+import com.app.killerapp.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.help, menu);
+		return true;
+	}
+
+}
