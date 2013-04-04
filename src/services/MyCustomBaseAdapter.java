e71d8cbfc1dafeab45efc9fb7082eb44c9da9074
Pieter Schouten <pieterschouten@live.nl>
2013-04-04 16:06:37 +0200

    Nog meer service shit

---
+++ src/services/MyCustomBaseAdapter.java
@@ -0,0 +1,64 @@
+package services;
+
+import java.util.ArrayList;
+
+import com.app.killerapp.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyCustomBaseAdapter extends BaseAdapter {
+	private static ArrayList<ServicesList> searchArrayList;
+
+	private LayoutInflater mInflater;
+
+	public MyCustomBaseAdapter(Context context, ArrayList<ServicesList> results) {
+		searchArrayList = results;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	public int getCount() {
+		return searchArrayList.size();
+	}
+
+	public Object getItem(int position) {
+		return searchArrayList.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.custom_row_view, null);
+			holder = new ViewHolder();
+			holder.txtName = (TextView) convertView.findViewById(R.id.name);
+			holder.txtCityState = (TextView) convertView
+					.findViewById(R.id.cityState);
+			holder.txtPhone = (TextView) convertView.findViewById(R.id.phone);
+
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		holder.txtName.setText(searchArrayList.get(position).getName());
+		holder.txtCityState.setText(searchArrayList.get(position)
+				.getOtherNumbers());
+		holder.txtPhone.setText(searchArrayList.get(position).getPhone());
+
+		return convertView;
+	}
+
+	static class ViewHolder {
+		TextView txtName;
+		TextView txtCityState;
+		TextView txtPhone;
+	}
+}
