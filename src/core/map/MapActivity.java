c5a5196c6c50d850df08badac86ac1868419c39b
Vincent Karsten <vincent.karsten@hva.nl>
2013-04-04 22:22:04 +0200

    show an example place on the map

--- src/core/map/MapActivity.java
+++ src/core/map/MapActivity.java
@@ -93,6 +93,7 @@ public class MapActivity extends Activity implements IRegisterReceiver {
         setContentView(mapView);
         
         addEvents();
+        addLocations();
 	}
 	
 	private void addLocations(){
--- src/core/place/PlaceUtil.java
+++ src/core/place/PlaceUtil.java
@@ -2,13 +2,15 @@ package core.place;
 
 import java.util.ArrayList;
 
+import org.osmdroid.util.GeoPoint;
+
 import core.models.Place;
 
 public class PlaceUtil {
 	public static ArrayList<Place> getDummyData(){
 		ArrayList<Place> location = new ArrayList<Place>();
 		
-		Place locationA = new Place();
+		Place locationA = new Place("De dam", "Dit is de dam. Hier is het heel interessant!", new GeoPoint(52.372823, 4.893626));
 		location.add(locationA);
 		
 		return location;
