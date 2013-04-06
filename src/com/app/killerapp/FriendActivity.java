1a42d43df4e1693ab83a1002298870693cf9d90c
Pieter Schouten <pieterschouten@live.nl>
2013-04-06 16:11:09 +0200

    Friend Activity layout aangepast
    De top bar voor de knoppen en de lijst voor het overzicht.
    Ook de dialog aangemaakt voor het toevoegen van companions

--- src/com/app/killerapp/FriendActivity.java
+++ src/com/app/killerapp/FriendActivity.java
@@ -1,7 +1,15 @@
 package com.app.killerapp;
 
+import java.util.regex.Pattern;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnDismissListener;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FriendActivity extends Activity{
 	@Override
@@ -10,4 +18,53 @@ public class FriendActivity extends Activity{
 		setContentView(R.layout.activity_friend);
 		
 	}
+}
+
+class AddCompanionDialog implements OnDismissListener, OnCancelListener {
+    final private EditText editText;
+    final private AlertDialog alertDialog;
+
+    private Boolean canceled;
+
+    AddCompanionDialog(Context context) {
+        editText = new EditText(context);
+        alertDialog = buildAlertDialog(context);
+        alertDialog.setOnDismissListener(this);
+        alertDialog.setOnCancelListener(this);
+        show();
+    }
+
+    private AlertDialog buildAlertDialog(Context context) {
+        return new AlertDialog.Builder(context)
+        .setTitle("Title")
+        .setMessage("Enter email address")
+        .setView(editText)
+        .setNeutralButton("Submit", null)
+        .setNegativeButton("Cancel", null)
+            .create();
+    }
+
+    public void show() {
+        canceled = false;
+        alertDialog.show();
+    }
+
+    @Override public void onDismiss(DialogInterface dialog) {
+        if(!canceled) {
+            final String name = editText.getText().toString();
+            if(!rfc2822.matcher(name).matches()) {
+                editText.setError("Email address is not valid");
+                show();
+            } else {
+                // Send request to server
+            }
+        }
+    }
+
+    @Override public void onCancel(DialogInterface dialog) {
+        canceled = true;
+    }
+    
+    private static final Pattern rfc2822 = Pattern
+			.compile("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
 }
