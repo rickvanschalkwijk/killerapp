8582e3645cf967fe976673dcbc3aef83158ca25b
Vincent Karsten <vincent.karsten@hva.nl>
2013-04-11 16:36:30 +0200

    events are clickable

--- src/core/event/EventList.java
+++ src/core/event/EventList.java
@@ -3,21 +3,28 @@ package core.event;
 import java.util.ArrayList;
 import java.util.List;
 
+import services.MyCustomBaseAdapter;
+import services.ServicesList;
+
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.app.killerapp.R;
 
 import core.databasehandlers.EventDataSource;
 import core.models.Event;
 
-public class EventList extends ListActivity {
+public class EventList extends Activity {
 
 	public static List<Event> events = new ArrayList<Event>();
 
@@ -31,48 +38,31 @@ public class EventList extends ListActivity {
 		events.clear();
 		events = eventDataSource.getAllEvents();
 		eventDataSource.close();
+		
+		final ListView listView = (ListView) findViewById(R.id.listEvents);
 
 		ArrayAdapter<Event> adapter = new ArrayAdapter<Event>(this,
 				R.layout.text_view, events);
-		setListAdapter(adapter);
-		/*
-		ListView listView = getListView();
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		listView.setAdapter(adapter);
+		
 
+		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
-				onListItemClick(v,pos,id);			
+			public void onItemClick(AdapterView<?> a, View v, int position,
+					long id) {
+				Dialog dialog = new Dialog(EventList.this);
+				dialog.setContentView(R.layout.event_dialog);
+				dialog.setTitle("Detailed event info");
+				dialog.setCancelable(true);
+
+				TextView name = (TextView) dialog.findViewById(R.id.textView1);
+				name.setText(events.get(position).getTitle());
+
+				TextView description = (TextView) dialog.findViewById(R.id.textView2);
+				description.setText(events.get(position).getDescription());
+				
+				dialog.show();
 			}
-        	
 		});
-		*/
 	}
-	
-	public void onListItemClick(ListView parent, View v,int position, long id) {
-		Dialog dialog = new Dialog(EventList.this);
-		dialog.setContentView(R.layout.event_dialog);
-		dialog.setTitle("Detailed event info");
-		dialog.setCancelable(true);
-
-		TextView name = (TextView) dialog.findViewById(R.id.textView1);
-		name.setText(events.get(position).getTitle());
-
-		TextView description = (TextView) dialog.findViewById(R.id.textView2);
-		description.setText(events.get(position).getDescription());
-	  }
-	
-	/*
-	protected void onListItemClick(View v, int pos, long id) {
-		Dialog dialog = new Dialog(EventList.this);
-		dialog.setContentView(R.layout.event_dialog);
-		dialog.setTitle("Detailed event info");
-		dialog.setCancelable(true);
-
-		TextView name = (TextView) dialog.findViewById(R.id.textView1);
-		name.setText(events.get(pos).getTitle());
-
-		TextView description = (TextView) dialog.findViewById(R.id.textView2);
-		description.setText(events.get(pos).getDescription());
-	}
-	*/
 }
