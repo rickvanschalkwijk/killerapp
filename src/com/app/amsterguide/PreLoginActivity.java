7c67c13239f21d8f16b7df5d7a5594f90d7de11e
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-04-11 13:20:25 +0200

    prelogin added image to background

--- src/com/app/amsterguide/PreLoginActivity.java
+++ src/com/app/amsterguide/PreLoginActivity.java
@@ -5,29 +5,32 @@ import com.app.killerapp.R.layout;
 import com.app.killerapp.R.menu;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class PreLoginActivity extends Activity {
 	private Context context = this;
 	
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 		setContentView(R.layout.activity_pre_login);
+		
 		Button signin = (Button) findViewById(R.id.btn_login);
-		Button register = (Button) findViewById(R.id.btnRegister);
+		Button register = (Button) findViewById(R.id.btn_register);
 		
 		signin.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent signinIntent = new Intent(context, LoginActivity.class);
-				finish();
 				startActivity(signinIntent);
 			}
 		});
@@ -35,7 +38,6 @@ public class PreLoginActivity extends Activity {
 		register.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent registerIntent = new Intent(context, RegistrationActivity.class);
-				finish();
 				startActivity(registerIntent);
 			}
 		});
