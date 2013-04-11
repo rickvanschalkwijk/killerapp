5b6290b7e8ecd78a2e50f6e4a0fae407e26a1cae
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-04-11 12:48:10 +0200

    added background image for prelogin activity

--- src/com/app/amsterguide/PreLoginActivity.java
+++ src/com/app/amsterguide/PreLoginActivity.java
@@ -6,14 +6,39 @@ import com.app.killerapp.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PreLoginActivity extends Activity {
-
+	private Context context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pre_login);
+		Button signin = (Button) findViewById(R.id.btn_login);
+		Button register = (Button) findViewById(R.id.btnRegister);
+		
+		signin.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent signinIntent = new Intent(context, LoginActivity.class);
+				finish();
+				startActivity(signinIntent);
+			}
+		});
+		
+		register.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent registerIntent = new Intent(context, RegistrationActivity.class);
+				finish();
+				startActivity(registerIntent);
+			}
+		});
 	}
 
 	@Override
