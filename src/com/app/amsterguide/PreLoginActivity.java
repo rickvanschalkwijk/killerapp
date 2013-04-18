7b26c2ffb0245abe70e51d421552d0b4985ac8fb
Onno Valkering <onno.valkering@gmail.com>
2013-04-18 15:27:26 +0200

    Revert "changed buttons locations prelogin"
    This reverts commit b3f51895ae4ac531266804bb7f8a5cbf103673d4.

--- src/com/app/amsterguide/PreLoginActivity.java
+++ src/com/app/amsterguide/PreLoginActivity.java
@@ -18,7 +18,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -64,14 +63,7 @@ public class PreLoginActivity extends Activity {
 		showLogin.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				LinearLayout loginLayout = (LinearLayout) findViewById(R.id.loginLayout);
-				if (loginLayout.getVisibility() == LinearLayout.GONE)
-				{
-					loginLayout.setVisibility(LinearLayout.VISIBLE);
-				}
-				else
-				{
-					loginLayout.setVisibility(LinearLayout.GONE);
-				}
+				loginLayout.setVisibility(LinearLayout.VISIBLE);
 			}
 		});
 
