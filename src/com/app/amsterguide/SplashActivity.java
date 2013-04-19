00f65578b1e967c0ce4cb6b5c4408da85ea174cc
Vincent Karsten <vincent.karsten@hva.nl>
2013-04-19 19:08:03 +0200

    remove the gps loader. It will crash the app

--- src/com/app/amsterguide/SplashActivity.java
+++ src/com/app/amsterguide/SplashActivity.java
@@ -55,9 +55,6 @@ public class SplashActivity extends Activity {
 			public void run(){
 				try {
 					synchronized (this) {
-						if(!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-							buildAlertNoGPS();
-						}
 						wait(_splashTime);
 					}
 				} catch (InterruptedException e) {
@@ -72,23 +69,7 @@ public class SplashActivity extends Activity {
 		splashThread.start();
 	}
 	
-	private void buildAlertNoGPS() {
-		 final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		    builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-		           .setCancelable(false)
-		           .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-		               public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-		                   startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-		               }
-		           })
-		           .setNegativeButton("No", new DialogInterface.OnClickListener() {
-		               public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-		                    dialog.cancel();
-		               }
-		           });
-		    final AlertDialog alert = builder.create();
-		    alert.show();
-	}
+	
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
 		if(event.getAction() == MotionEvent.ACTION_DOWN){
