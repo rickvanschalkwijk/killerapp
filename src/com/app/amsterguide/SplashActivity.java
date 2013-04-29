900f2fb111999b219cbd11f982d3ed7875ae6200
Vincent Karsten <vincent.karsten@hva.nl>
2013-04-29 18:15:19 +0200

    final product

--- src/com/app/amsterguide/SplashActivity.java
+++ src/com/app/amsterguide/SplashActivity.java
@@ -79,40 +79,44 @@ public class SplashActivity extends Activity {
 		};
 		splashThread.start();
 	}
-	
+
 	private void copyMapToDevice() {
-	    AssetManager assetManager = getAssets();
-	    String[] files = null;
-	    try {
-	        files = assetManager.list("");
-	    } catch (IOException e) {
-	        Log.e("tag", "Failed to get asset file list.", e);
-	    }
-	    for(String filename : files) {
-	        InputStream in = null;
-	        OutputStream out = null;
-	        try {
-	          in = assetManager.open(filename);
-	          out = new FileOutputStream(Environment.getExternalStorageDirectory() + java.io.File.separator + filename);
-	          copyFile(in, out);
-	          in.close();
-	          in = null;
-	          out.flush();
-	          out.close();
-	          out = null;
-	        } catch(IOException e) {
-	            Log.e("tag", "Failed to copy asset file: " + filename, e);
-	        }       
-	    }
+		AssetManager assetManager = getAssets();
+		String[] files = null;
+		try {
+			files = assetManager.list("");
+		} catch (IOException e) {
+			Log.e("tag", "Failed to get asset file list.", e);
+		}
+		for (String filename : files) {
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				in = assetManager.open(filename);
+				out = new FileOutputStream(
+						Environment.getExternalStorageDirectory()
+								+ java.io.File.separator + filename);
+
+				copyFile(in, out);
+				in.close();
+				in = null;
+				out.flush();
+				out.close();
+				out = null;
+			} catch (IOException e) {
+				Log.e("tag", "Failed to copy asset file: " + filename, e);
+			}
+		}
 	}
+
 	private void copyFile(InputStream in, OutputStream out) throws IOException {
-	    byte[] buffer = new byte[1024];
-	    int read;
-	    while((read = in.read(buffer)) != -1){
-	      out.write(buffer, 0, read);
-	    }
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+		}
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
